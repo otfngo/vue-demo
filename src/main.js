634e99400ba68a037629ed5f97bb51bc30a5bdd1
otfngo <otfngo@163.com>
2018-07-06 18:04:52 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import '@/directive/vue-directive.js'
-import VueTouch from 'vue-touch'
 
-Vue.use(VueTouch)
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
