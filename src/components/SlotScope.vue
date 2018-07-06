5b86e1acfb716c6f9a060214ab9841ad65b87bf2
otfngo <otfngo@163.com>
2018-07-07 00:31:00 +0800

    ...

---
+++ src/components/SlotScope.vue
@@ -0,0 +1,23 @@
+<template>
+  <todo-list :todos="todos"></todo-list>
+</template>
+
+<script>
+import TodoList from './TodoList'
+
+export default {
+  comments: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'text1' },
+        { id: 2, text: 'text2' },
+        { id: 3, text: 'text3' },
+        { id: 4, text: 'text4' }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
